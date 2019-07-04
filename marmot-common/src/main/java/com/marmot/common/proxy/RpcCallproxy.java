ad56e84bc6dc025633538b87e9aeeb140541d1cc
WSD\miaozs <miaozhengsheng@126.com>
2019-07-04 20:40:11 +0800

    实现简单的RPC

---
+++ marmot-common/src/main/java/com/marmot/common/proxy/RpcCallproxy.java
@@ -0,0 +1,19 @@
+package com.marmot.common.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class RpcCallproxy implements InvocationHandler{
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args)
+			throws Throwable {
+		
+		// 发送http请求到远程
+		
+		
+		
+		return null;
+	}
+
+}
