d57678e47252892a02d3451ff2f10de8aeb8005c
WSD\miaozs <miaozhengsheng@126.com>
2019-07-05 20:30:27 +0800

    初始化框架

---
+++ marmot-common/src/main/java/com/marmot/common/rpc/hystrix/HystrixCommandCallWrapper.java
@@ -0,0 +1,28 @@
+package com.marmot.common.rpc.hystrix;
+
+
+import java.lang.reflect.Method;
+
+import com.netflix.hystrix.HystrixCommand;
+
+public class HystrixCommandCallWrapper extends HystrixCommand<Object> {
+	
+	
+	protected Method method;
+    protected Object[] args;
+
+    public HystrixCommandCallWrapper(Method method, Object[] args) {
+        super(HystrixConfigurationCurator.getInstance().getClientHystrixCommandSetter(
+                serviceMetadata.getProjectName(), serviceMetadata.getHystrixCommandKey(method, args),
+                serviceMetadata.isTimeout(method)));
+        this.method = method;
+        this.args = args;
+    }
+
+	@Override
+	protected Object run() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
