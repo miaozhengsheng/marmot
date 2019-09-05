db661ca758143a733f829ea45fb3b7f8eaac42fe
WSD\miaozs <miaozhengsheng@126.com>
2019-09-05 10:20:38 +0800

    功能完善

--- springmvc/src/test/java/com/liepin/eadc/erp/demo/springmvc/demo/Test.java
+++ springmvc/src/test/java/com/liepin/eadc/erp/demo/springmvc/demo/Test.java
@@ -12,7 +12,7 @@ public class Test {
     	
     	IMarmotDemoService proxy = RpcCallProxy.getProxy(IMarmotDemoService.class);
     	
-    	for(int i=0;i<20;i++){
+    	for(int i=0;i<1;i++){
 	    	Integer result = proxy.add(1, 2);
 	    	
 	    	System.out.println(result);
