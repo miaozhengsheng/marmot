802eb532f9bac185965a44c7c87d41fee4f13536
WSD\miaozs <miaozhengsheng@126.com>
2019-10-25 20:25:09 +0800

    refactor kafka message code

--- marmot-common/src/main/java/com/marmot/common/component/IMarmotComponent.java
+++ marmot-common/src/main/java/com/marmot/common/component/IMarmotComponent.java
@@ -6,4 +6,5 @@ public interface IMarmotComponent {
 
 	
 	public void initComponent(ApplicationContext applicationContext);
+	
 }
--- marmot-kafka/src/main/java/com/marmot/com/marmot/kafka/KafkaConsumer.java
+++ marmot-kafka/src/main/java/com/marmot/com/marmot/kafka/KafkaConsumer.java
@@ -19,8 +19,6 @@ import kafka.javaapi.consumer.ConsumerConnector;
 import kafka.serializer.StringDecoder;
 import kafka.utils.VerifiableProperties;
 
-import org.apache.log4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
@@ -29,97 +27,48 @@ import org.springframework.stereotype.Component;
 import com.marmot.com.marmot.kafka.topic.IKafkaTopic;
 import com.marmot.common.component.IMarmotComponent;
 import com.marmot.common.conf.ClientUtil;
-import com.marmot.common.log.BusinessLog;
-import com.marmot.common.system.SystemUtil;
-import com.marmot.common.util.PropUtil;
 
 @Component
-public class KafkaConsumer implements IMarmotComponent, InitializingBean,
-		DisposableBean {
+public class KafkaConsumer implements IMarmotComponent, DisposableBean {
 
-	/**
-	 * 
-	 */
+	private ExecutorService service = null;
 
-	private  ExecutorService service = null;
-
-	private static ConsumerConnector consumer;
-
+	private static  ConsumerConnector connector ;
 
 	@Override
 	public void initComponent(ApplicationContext applicationContext) {
-		
+
 		Map<String, IKafkaTopic> beansByType = applicationContext
 				.getBeansOfType(IKafkaTopic.class);
 
 		if (beansByType == null || beansByType.isEmpty()) {
 			return;
 		}
-
+		connector = initConsumer();
 		Iterator<Entry<String, IKafkaTopic>> iterator = beansByType.entrySet()
 				.iterator();
 
 		service = new ThreadPoolExecutor(beansByType.size(), 10, 3000,
-					TimeUnit.MICROSECONDS, new ArrayBlockingQueue<Runnable>(32));
-		
+				TimeUnit.MICROSECONDS, new ArrayBlockingQueue<Runnable>(32));
+
 		while (iterator.hasNext()) {
 
-			
 			Entry<String, IKafkaTopic> next = iterator.next();
 
 			String topic = next.getKey();
 
 			topic = topic.substring(topic.indexOf("KAFKA.TOPIC.") + 12);
-			
+
 			service.submit(new KafKaExecutor(next.getValue(), topic));
 		}
 	}
 
-	private static class KafKaExecutor implements Callable<Void> {
-		private IKafkaTopic kafkaConsume;
-
-		private String topic;
-
-		public KafKaExecutor(IKafkaTopic kafkaConsume, String topic) {
-			this.kafkaConsume = kafkaConsume;
-			this.topic = topic;
-
-		}
-
-		@Override
-		public Void call() throws Exception {
-			
-			Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-			topicCountMap.put(topic, new Integer(1));
-
-			StringDecoder keyDecoder = new StringDecoder(new VerifiableProperties());
-			StringDecoder valueDecoder = new StringDecoder(
-					new VerifiableProperties());
-
-			Map<String, List<KafkaStream<String, String>>> consumerMap = consumer
-					.createMessageStreams(topicCountMap, keyDecoder, valueDecoder);
-			KafkaStream<String, String> stream = consumerMap.get(topic).get(0);
-			ConsumerIterator<String, String> it = stream.iterator();
-			while (it.hasNext()){
-				// 消费消息
-				kafkaConsume.consumeMessage(it.next().message());
-			}
-			
-			return null;
-		}
-
-	}
-
-	@Override
-	public void destroy() throws Exception {
-		service.shutdown();
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
+	private ConsumerConnector initConsumer() {
+		// 初始化 链接
 		Properties props = new Properties();
 		// zookeeper 配置
-		props.put("zookeeper.connect", "192.168.117.130:2181,192.168.117.131:2181,192.168.117.132:2181");
+		props.put("zookeeper.connect",
+				"192.168.117.130:2181,192.168.117.131:2181,192.168.117.132:2181");
 
 		props.put("group.id", ClientUtil.getProjectName());
 
@@ -133,7 +82,53 @@ public class KafkaConsumer implements IMarmotComponent, InitializingBean,
 
 		ConsumerConfig config = new ConsumerConfig(props);
 
-		consumer = kafka.consumer.Consumer.createJavaConsumerConnector(config);
+		ConsumerConnector consumer = kafka.consumer.Consumer
+				.createJavaConsumerConnector(config);
+
+		return consumer;
+	}
+
+	private static class KafKaExecutor implements Callable<Void> {
+
+		private IKafkaTopic kafkaConsume;
+
+		private String topic;
+
+		public KafKaExecutor(IKafkaTopic kafkaConsume, String topic) {
+			this.kafkaConsume = kafkaConsume;
+			this.topic = topic;
+
+		}
+
+		@Override
+		public Void call() throws Exception {
+
+			Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+			topicCountMap.put(topic, new Integer(1));
+
+			StringDecoder keyDecoder = new StringDecoder(
+					new VerifiableProperties());
+			StringDecoder valueDecoder = new StringDecoder(
+					new VerifiableProperties());
+
+			Map<String, List<KafkaStream<String, String>>> consumerMap = connector
+					.createMessageStreams(topicCountMap, keyDecoder,
+							valueDecoder);
+			KafkaStream<String, String> stream = consumerMap.get(topic).get(0);
+			ConsumerIterator<String, String> it = stream.iterator();
+			while (it.hasNext()) {
+				// 消费消息
+				kafkaConsume.consumeMessage(it.next().message());
+			}
+
+			return null;
+		}
+
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		service.shutdown();
 	}
 
 }
